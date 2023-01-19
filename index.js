104fe57d40208c4308bdecb8577def70457ee62a
guillemreig <guillemreiggrau@gmail.com>
2023-01-19 20:41:39 +0100

    Pushing just in case

--- index.js
+++ index.js
@@ -45,6 +45,16 @@ import http from "http";
 const server = http.createServer(app);
 import { Server } from "socket.io";
 
+// CORS
+import cors from "cors";
+const corsOptions = {
+    origin: "*",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
+
 const io = new Server(server);
 
 // const server = require("http").Server(app);
