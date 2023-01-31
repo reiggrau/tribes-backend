1a1f5c084d24448abee1bd04233576cff516594b
guillemreig <guillemreiggrau@gmail.com>
2023-01-31 11:54:38 +0100

    Push before deploy

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ import cookieSession from "cookie-session";
 const cookieSessionMiddleware = cookieSession({
     name: "session",
     secret: process.env.SESSION_SECRET,
+    keys: process.env.SESSION_SECRET,
     maxAge: 1000 * 60 * 60 * 24 * 1, // miliseconds * seconds * minutes * hours * days // currently: 1 day
     secure: true,
     sameSite: "none",
